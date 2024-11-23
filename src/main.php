1a32646e593204c52d64882f31c75c6ea4e5e6cf
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-11-23 17:59:34 +0900

    feat: autoloadの設定

--- src/main.php
+++ src/main.php
@@ -1,11 +1,23 @@
 <?php
 
-require_once 'Interfaces/Engine.php';
-require_once 'Engines/GasolineEngine.php';
-require_once 'Engines/ElectricEngine.php';
-require_once 'Cars/Car.php';
-require_once 'Cars/GasCar.php';
-require_once 'Cars/ElectricCar.php';
+// require_once 'Interfaces/Engine.php';
+// require_once 'Engines/GasolineEngine.php';
+// require_once 'Engines/ElectricEngine.php';
+// require_once 'Cars/Car.php';
+// require_once 'Cars/GasCar.php';
+// require_once 'Cars/ElectricCar.php';
+
+spl_autoload_register(function ($name) {
+    // __DIR__は、現在のファイルの絶対ディレクトリパスを取得します。
+    $filepath = __DIR__ . "/" . str_replace('\\', '/', $name) . ".php";
+    echo "\nRequiring...." . $name . " once ($filepath).\n" . '<br>';
+    // バックスラッシュ(\)をフロントスラッシュ(/)に置き換えます。フロントスラッシュはLinuxのファイルパスで使用されます。
+    require_once $filepath;
+});
+
+
+// spl_autoload_extensions(".php"); 
+// spl_autoload_register();
 
 $gasCar = new Cars\GasCar('Toyota');
 $electricCar = new Cars\ElectricCar('Tesla');
