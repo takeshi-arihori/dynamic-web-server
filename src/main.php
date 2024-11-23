43ef9de8030a1a7b1c6fe985466b5dbc2ddda5f2
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-11-23 19:43:47 +0900

    feat: header情報の確認

--- src/main.php
+++ src/main.php
@@ -1,3 +1,33 @@
 <?php
+$planet = isset($_GET['planet']) ? $_GET['planet'] : 'Earth';
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-echo phpinfo();
+<head>
+    <title>Hello, World!</title>
+</head>
+
+<body>
+    <h1>Hello, <?php echo htmlspecialchars($planet, ENT_QUOTES, 'UTF-8'); ?></h1>
+    <p>This is a simple "Hello, <?php echo htmlspecialchars($planet, ENT_QUOTES, 'UTF-8'); ?>!" HTML page.</p>
+    <?php
+    // リクエストヘッダーを取得
+    $headers = getallheaders();
+
+    echo json_encode($headers);
+
+    // // JSON形式で出力
+    // header('Content-Type: application/json');
+    // $filename = 'request_headers.json';
+    // header(sprintf('Content-Disposition: attachment; filename="%s"', $filename));
+    // echo json_encode($headers, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    // exit; // ここで終了してHTMLのフッターを出力しない
+    ?>
+
+    <footer>
+        <p><?php echo sprintf("%s Rocks© Website %s. All rights reserved.", htmlspecialchars($planet, ENT_QUOTES, 'UTF-8'), date("Y")); ?></p>
+    </footer>
+</body>
+
+</html>
