a090a54a020bf59839fae659246d61d9bdef3ea0
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-11-23 14:23:16 +0900

    feat: OOPの復習 カプセル化

--- src/index.php
+++ src/index.php
@@ -1,3 +1,46 @@
 <?php
 
-echo "Hello PHP root";
+class Car
+{
+    private string $make;
+    private string $model;
+    private int $year;
+
+    public function __construct(string $make, string $model, int $year)
+    {
+        $this->make = $make;
+        $this->model = $model;
+        $this->year = $year;
+    }
+
+    public function getMake(): string
+    {
+        return $this->make;
+    }
+
+    public function getModel(): string
+    {
+        return $this->model;
+    }
+
+    public function getYear(): int
+    {
+        return $this->year;
+    }
+
+    public function calculateAge(): int
+    {
+        $currentYear = date('Y');
+        return $this->getAge($currentYear);
+    }
+
+    public function getAge(int $currentYear): int
+    {
+        return $currentYear - $this->year;
+    }
+}
+
+
+$car = new Car('Toyota', 'Camry', 2022);
+echo "Car Details: " . $car->getMake() . " " . $car->getModel() . " (" . $car->getYear() . ")\n";
+echo "Car Age: " . $car->calculateAge() . " years"; // Output: Car Age: 1 years
